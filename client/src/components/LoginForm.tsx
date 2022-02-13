bd58016d405d69c9242083dee16920430c895c59
Schekhovtsov <schekhovtsov@gmail.com>
2022-02-13 22:01:42 +0300

    RHF (don't working)

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,17 +1,18 @@
 import { observer } from 'mobx-react-lite';
 import React, { FC, useContext, useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { AuthContext } from '..';
 import { IUser } from '../models/IUser';
 import UserService from '../services/UserService';
 
-/* interface IFormInput {
+interface IFormInput {
   email: String;
   password: String;
-} */
+}
 
 export const LoginForm: FC = observer(() => {
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
+/*   const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>(''); */
   const { store } = useContext(AuthContext);
 
   const [users, setUsers] = useState<IUser[]>([]);
@@ -25,23 +26,29 @@ export const LoginForm: FC = observer(() => {
     }
   };
 
-  const handleLogin = () => {
+/*   const handleLogin = () => {
     store.login(email, password);
   };
 
   const handleRegister = () => {
     store.registration(email, password);
-  };
+  }; */
 
   const handleLogout = () => {
     store.logout();
+  };
+
+  const { register, handleSubmit } = useForm<IFormInput>();
+
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    store.login(data.email, data.password);
   };
 
   return (
     <div>
       {!store.isAuth ? (
         <div>
-          <input
+          {/*  <input
             type="text"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
@@ -54,7 +61,14 @@ export const LoginForm: FC = observer(() => {
             placeholder="Password"
           />
           <button onClick={handleLogin}>Login</button>
-          <button onClick={handleRegister}>Register</button>
+          <button onClick={handleRegister}>Register</button> */}
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <label>E-mail</label>
+            <input {...(register('email'))} />
+            <label>Password</label>
+            <input {...register('password')} />
+            <input type="submit" />
+          </form>
         </div>
       ) : (
         <div>
@@ -65,21 +79,4 @@ export const LoginForm: FC = observer(() => {
       )}
     </div>
   );
-
-  /*   const { register, handleSubmit } = useForm<IFormInput>();
-
-  const onSubmit: SubmitHandler<IFormInput> = (data) => {
-    data.password = '1488';
-    console.log(data);
-  };
-
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label>E-mail</label>
-      <input {...(register('email'), { required: true })} />
-      <label>Password</label>
-      <input {...register('password')} />
-      <input type="submit" />
-    </form>
-  ); */
 });
